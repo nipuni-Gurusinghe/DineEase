8c8c85674772ca3bb78b53f18bf4364fbd0291dc
nipuni-Gurusinghe <amashagurusinghe2003@gmail.com>
2025-09-25 12:25:58 +0530

    Add regular order to the end of queue,Add immediate order to the front of queue

--- src/OrderQueueManager.java
+++ src/OrderQueueManager.java
@@ -1,2 +1,39 @@
+
+import java.util.*;
+
 public class OrderQueueManager {
+    private LinkedList<Order> orderQueue;
+    private int nextOrderId;
+
+    public OrderQueueManager() {
+        this.orderQueue = new LinkedList<>();
+        this.nextOrderId = 1;
+    }
+
+
+    public void addOrder(String customerName, String orderDetails) {
+        Order order = new Order(nextOrderId++, customerName, orderDetails, false);
+        orderQueue.addLast(order);
+        System.out.println("Added: " + order);
+    }
+
+
+    public void addImmediateOrder(String customerName, String orderDetails) {
+        Order order = new Order(nextOrderId++, customerName, orderDetails, true);
+
+
+        int insertPosition = 0;
+        for (Order o : orderQueue) {
+            if (o.isImmediate()) {
+                insertPosition++;
+            } else {
+                break;
+            }
+        }
+
+        orderQueue.add(insertPosition, order);
+        System.out.println("Added Immediate: " + order);
+    }
+
+
 }
